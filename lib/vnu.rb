ee550d874f570f4870fa7d6f1339022d6cb86ac4
Sven Dahlstrand <sven@dahlstrand.net>
2016-08-29 23:59:29 +0200

    Update custom test for compatibility.

--- lib/vnu.rb
+++ lib/vnu.rb
@@ -1,13 +1,13 @@
 require 'vnu'
 require 'json'
 
-class VnuCheck < ::HTMLProofer::Runner
+class VnuCheck < ::HTMLProofer::Check
   def run
-    error = Vnu.validate(@html, errors_only: true, format: 'json')
+    errors = Vnu.validate(@html, errors_only: true, format: 'json')
 
-    if error
-      JSON.parse(error)['messages'].each do |error|
-        add_issue(error['message'], error['lastLine'])
+    if errors
+      JSON.parse(errors)['messages'].each do |error|
+        add_issue(error['message'], line: error['lastLine'])
       end
     end
   end
